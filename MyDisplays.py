0cc06835b4c307f91397a67f2b3e6612d6c5abfa
CWP -- pi-radio <cwgphillips@gmail.com>
2023-12-18 12:33:21 +0000

    #2 -- code tidy

--- MyDisplays.py
+++ MyDisplays.py
@@ -1,9 +1,6 @@
-from random import randint
-import sys, os
-import time
+import os
 import ST7789 as ST7789
-from PIL import Image, ImageDraw, ImageOps
-from colorsys import hsv_to_rgb
+from PIL import Image, ImageDraw
 import cairosvg
 
 class SqauareDisplay:
@@ -21,6 +18,7 @@ class SqauareDisplay:
         "play": "https://www.reshot.com/preview-assets/icons/M5CZEU4XWN/play-M5CZEU4XWN.svg",
         "stop": "https://www.reshot.com/preview-assets/icons/6TMKY3BGJX/stop-6TMKY3BGJX.svg",
     }
+
 
     def __init__(self) -> None:
 
@@ -42,6 +40,7 @@ class SqauareDisplay:
         self.image = self._parseImagePath(image_path)
         self.disp.display(self.image)
 
+
     def show_composite(self, mainImagePath, upperLeftPath, upperRightPath, lowerLeftPath, lowerRightPath):
         imageMain = self._parseImagePath(mainImagePath)
         imageUppLeft = self._parseImagePath(upperLeftPath)
@@ -58,43 +57,44 @@ class SqauareDisplay:
         imageMain.paste(imageUppLeft, (0,0), imageUppLeft)
         imageMain.paste(imageLowLeft, (0,190), imageLowLeft)
         imageMain.paste(imageLowRight, (190,190), imageLowRight)
-        
+
         self.image = imageMain
         self.disp.display(self.image)
 
+
     def _parseImagePath(self, image_path):
-        print(f"\t### image_path: {image_path}")
         if type(image_path) is list or type(image_path) is tuple:
             if image_path[0][:4]=="http":
                 return self.get_from_url(image_path)
-            elif image_path[0][-4:] == '.png':
+            if image_path[0][-4:] == '.png':
                 return self.get_from_file(image_path)
-            elif image_path[0].lower() in list(self.icon_urls.keys()):
+            if image_path[0].lower() in list(self.icon_urls.keys()):
                 return self.get_from_url((self.icon_urls[image_path[0].lower()],image_path[1]))
-        
+
         elif image_path.lower() in list(self.icon_urls.keys()):
             return self.get_from_url((self.icon_urls[image_path.lower()],image_path))
-        
+
         elif image_path.lower()=='blank':
             base = Image.new("RGBA",(self.WIDTH, self.HEIGHT),(0,0,0,0))
             ImageDraw.Draw(base)
             return base
-        
+
         elif image_path is None:
             return None
+
 
     def get_from_file(self, image_file_name_in):
         image = Image.open(image_file_name_in)
         return image.resize((self.WIDTH, self.HEIGHT))
 
+
     def get_from_url(self, image_url_in):
         testDoesImageAlreadyExistLocally = f"{self.DIR_DOWNLOADED_IMAGES}/{image_url_in[1]}.png"
-        print(f"\t### testDoesImageAlreadyExistLocally: {testDoesImageAlreadyExistLocally}")
         if os.path.exists(testDoesImageAlreadyExistLocally):
-            print(f"\t### Does exist... testDoesImageAlreadyExistLocally: {testDoesImageAlreadyExistLocally}")
+            print(f"\t### Image exists... {testDoesImageAlreadyExistLocally}")
             imageToShowPath = testDoesImageAlreadyExistLocally
         else:
-            print(f"\t### Does not exist... testDoesImageAlreadyExistLocally: {testDoesImageAlreadyExistLocally}")
+            print(f"\t### Image does not exist... {testDoesImageAlreadyExistLocally}")
             image_type = os.path.splitext(os.path.basename(image_url_in[0]))[1]
             if not os.path.exists(self.DIR_DOWNLOADED_IMAGES):
                 os.mkdir(self.DIR_DOWNLOADED_IMAGES)
@@ -108,13 +108,12 @@ class SqauareDisplay:
                 imageToShowPath = downloaded_image
 
         print(f"Loading image: {imageToShowPath}")                        
-        
+
         return self.get_from_file(imageToShowPath)
+
 
     def my_svg2png(self, svgImagePath):
         pngImageOutPath = os.path.join(os.path.dirname(svgImagePath),\
              os.path.splitext(os.path.basename(svgImagePath))[0]+".png")
         cairosvg.svg2png(url=svgImagePath, write_to=pngImageOutPath)
         return pngImageOutPath
-        
-
--- Station.py
+++ Station.py
@@ -14,39 +14,18 @@ class Stations():
                             bbc_4.name: bbc_4, 
                             bbc_6.name: bbc_6
                             }
- 
+
+
     def station_names(self):
         return list(self.station_dictionary.keys())
-    
+
+
     def station_for_display(self, station_name):
         return (self.station_dictionary[station_name].path_icon, station_name)
+
 
 class _Station():
     def __init__(self, name, path_m3u8, path_icon ) -> None:
         self.name = name
         self.path_m3u8 = path_m3u8
         self.path_icon = path_icon
-
-    # def get_name(self):
-    #     return self.name
-
-    # def get_path_icon(self):
-    #     print(f"### Icon Path: {self.path_icon}")
-    #     return self.path_icon
-
-    # def get_path_m3u8(self):
-    #     return self.path_m3u8
-
-# def get_station_list():
-#     bbc_4 = Station( "bbc_4.m3u8", "https://sounds.files.bbci.co.uk/3.1.5/networks/bbc_radio_four/colour_default.svg")
-#     bbc_6_Music = Station( "bbc_6.m3u8", "https://sounds.files.bbci.co.uk/3.1.5/networks/bbc_6music/colour_default.svg")
-    
-#     Station_list = {"bbc_4": bbc_4, 
-#                     "bbc_6": bbc_6_Music
-#                     }
-    
-#     return Station_list
-
-
-
-        
